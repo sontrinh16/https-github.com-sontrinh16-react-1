5c70a517f5c9c154f8a4dc10a70e059debf61b2f
sontrinh16 <trinhleson2000@gmail.com>
2021-11-01 18:48:47 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-vd4msf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-vd4msf)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ListHeroes from './components/ListHeroes';
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <ListHeroes/>
+    </div>
+  );
+}
+
+export default App;
